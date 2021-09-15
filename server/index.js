beb399ef332e2becbaf5e8b1956dafc16dc578bb
MrDraong <capiauxcharles49@gmail.com>
2021-09-15 20:35:50 +0200

    modifications server side

--- server/index.js
+++ server/index.js
@@ -9,8 +9,13 @@ const io = require('socket.io')(server);
 
 let users = [];
 
+const messages = {
+  general: [],
+  game: [],
+};
+
 io.on('connection', socket => {
-  io.emit('userid',socket.id);
+
   socket.on("join server", username => {
     const user = {
       username,
@@ -19,7 +24,44 @@ io.on('connection', socket => {
     users.push(user);
     io.emit("new user", users);
   });
-  socket.on('SEND_MESSAGE', function(data) {
-      io.emit('MESSAGE', data)
+
+  socket.on("join room", (roomName, callback) => {
+    socket.join(roomName);
+    callback(messages[roomName]);
+    socket.emit("joined", messages[roomName]);
   });
+
+  socket.on("send message", ({content, to, sender, chatName, isChannel}) =>{
+    if(isChannel){
+      const payload = {
+        content,
+        chatName,
+        sender
+      };
+      socket.to(to).emit("new message", payload);
+    }
+    else{
+      const payload = {
+          content,
+          chatName: sender,
+          sender
+      };
+      socket.to(to).emit("new message", payload);
+    }
+    if(messages[chatName]){
+      messages[chatName].push({
+        sender,
+        content
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("new user", users);
+  });
+
+  //socket.on('SEND_MESSAGE', function(data) {
+  //   io.emit('MESSAGE', data)
+  //});
 });
